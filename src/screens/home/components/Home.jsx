6bf551b844cf6063d897017b809e726e35967cf7
Hilda <hilda.kh.amiri@gmail.com>
2020-03-12 22:44:24 -0700

    modal import commented out

--- src/screens/home/components/Home.jsx
+++ src/screens/home/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../../../general/Header/Header';
-import Modal from '../../../general/Modal/Modal';
+// import Modal from '../../../general/Modal/Modal';
 import { Link, useHistory } from "react-router-dom"
 
 const Home = () => {
