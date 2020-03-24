b3153637819ff15564897fc3e9e88e90e224aa92
Jay Seo <sjs5953@hotmail.com>
2020-03-24 01:28:46 -0700

    react-week-calender try

--- src/screens/main/components/Main.jsx
+++ src/screens/main/components/Main.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import Header from '../../../general/Header/Header';
+import WeekCalendar from 'react-week-calendar';
+ 
+import 'react-week-calendar/dist/style.css'
+import moment from 'moment';
+
 const Main = (props) => {
   return (
     <div>
-      <Header
+      {/* <Header
         // color="transparent"
         brand="LHL SCHEDULER"
         fixed
         user={props.user}
         setUser={props.setUser}
+      /> */}
+
+      <WeekCalendar
+        firstDay={moment('2019W10')}
+        numberOfDays={7}
+        scaleHeaderTitle={'LHL Scheduler'}
+        scaleFormat= {'h:mm'}
+        startTime= {moment({ h: 10, m: 0 })}
+        endTime={moment({ h: 22, m: 0 })}
+        scaleUnit={60}
+        cellHeight={40}
+        eventSpacing={30}
       />
       
       
