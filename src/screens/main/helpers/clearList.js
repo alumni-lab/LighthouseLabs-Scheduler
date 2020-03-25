c9fcf343f07bee63bb15d5cefeaa375281943ded
Jay Seo <sjs5953@hotmail.com>
2020-03-25 03:00:07 -0700

    added clear column featuer

--- src/screens/main/helpers/clearList.js
+++ src/screens/main/helpers/clearList.js
@@ -1,7 +1,12 @@
-export default function clearList (columnId, columns, setColumns) {
+export default function clearList (dayId, columns, setColumns) {
   let newState = {...columns};
-  let deletedItems = newState[columnId].items
-  newState[columnId].items = [];
-  newState['list'].items = [...newState['list'].items, ...deletedItems]
+  // let deletedItems = newState[dayId].items
+  // newState['list'].items = [...newState['list'].items, ...deletedItems];
+  // newState[dayId].items
+  // setColumns(newState)
+  const target = newState[dayId].items;
+  for (let ele in target) {
+    target[ele].items=[]
+  }
   setColumns(newState)
 }
